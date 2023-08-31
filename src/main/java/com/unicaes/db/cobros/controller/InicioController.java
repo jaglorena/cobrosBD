f7c798c9f1f80aef734aa71081d6cfc9617319f2
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2023-08-31 09:49:58 -0600

    Agregando logica a transaccion - agregar producto

--- src/main/java/com/unicaes/db/cobros/controller/InicioController.java
+++ src/main/java/com/unicaes/db/cobros/controller/InicioController.java
@@ -1,14 +1,39 @@
 package com.unicaes.db.cobros.controller;
 
+import com.unicaes.db.cobros.enity.DetallesPromocion;
+import com.unicaes.db.cobros.enity.Producto;
+import com.unicaes.db.cobros.repository.ProductoRepository;
+import com.unicaes.db.cobros.repository.PromocionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class InicioController {
+    Producto producto;
+    DetallesPromocion detallesPromocion;
+    @Autowired
+    ProductoRepository productoRepository;
+    @Autowired
+    PromocionRepository promocionRepository;
+
 
     @RequestMapping()
     public String inicio(){
+
+        producto = new Producto();
+        producto.setNombre("Jabon tex");
+        producto.setCodigo("TX001");
+        producto.setDescripcion("Jabon lavatrastos");
+        producto.setPrecioUnitario(1.0);
+        productoRepository.save(producto);
+
+        detallesPromocion = new DetallesPromocion();
+        detallesPromocion.setNombre("10% de descuento");
+        detallesPromocion.setDescuentoAplicado(10.0);
+        promocionRepository.save(detallesPromocion);
+
         return "inicio";
     }
 }
--- src/main/java/com/unicaes/db/cobros/controller/TransaccionController.java
+++ src/main/java/com/unicaes/db/cobros/controller/TransaccionController.java
@@ -1,19 +1,63 @@
 package com.unicaes.db.cobros.controller;
 
+import com.unicaes.db.cobros.enity.DetallesPromocion;
+import com.unicaes.db.cobros.enity.Producto;
+import com.unicaes.db.cobros.enity.Transacciones;
+import com.unicaes.db.cobros.repository.ClienteRepository;
+import com.unicaes.db.cobros.repository.ProductoRepository;
+import com.unicaes.db.cobros.repository.PromocionRepository;
 import com.unicaes.db.cobros.repository.TransaccionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping ("/transacciones")
 public class TransaccionController {
     @Autowired
     TransaccionRepository repository;
-    @GetMapping ("")
-    public String listarTransaccion(){
-        return "transaccion";
+    @Autowired
+    ProductoRepository productoRepository;
+    @Autowired
+    ClienteRepository clienteRepository;
+    @Autowired
+    PromocionRepository promocionRepository;
 
+    @GetMapping ("")
+    public String listarTransaccion(Model model){
+        List<Producto> productos = (List<Producto>) productoRepository.findAll();
+        List<DetallesPromocion> promociones = (List<DetallesPromocion>) promocionRepository.findAll();
+        List<Transacciones> transacciones = (List<Transacciones>) repository.findAll();
+        model.addAttribute("productos", productos);
+        model.addAttribute("descuentos", promociones);
+        model.addAttribute("transacciones", transacciones);
+        return "transaccion";
+    }
+
+    @PostMapping("/agregar")
+    public String crearTransaccion(
+            @RequestParam(value = "idProducto") int idProducto,
+            @RequestParam(value = "cantidad") int cantidad,
+            @RequestParam(value = "idDescuento") int idDescuento
+    ) {
+        Optional<Producto> producto = productoRepository.findById(idProducto);
+        Optional<DetallesPromocion> promocion = promocionRepository.findById(idDescuento);
+        if (producto.isPresent()) {
+            Transacciones transacciones = new Transacciones();
+            transacciones.setCantidadVendida(cantidad);
+            transacciones.setProducto(producto.get());
+            transacciones.setFechaHora(LocalDateTime.now());
+            promocion.ifPresent(transacciones::setPromocion);
+            repository.save(transacciones);
+        }
+        return "redirect:/transacciones";
     }
 }
